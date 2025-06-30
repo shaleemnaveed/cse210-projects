3db32a0350d3f3619092b43e766bb2f6516adc16
shaleemnaveed <shaleemnaveed@gmail.com>
2025-06-30 22:17:15 +0500

    Finished Prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        while (true)
+        {
+            // Generate magic random number to guess
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int count = 0;
+            while (true)
+            {
+                // Get user's guess
+                Console.Write("What is your guess? ");
+                int userGuess = int.Parse(Console.ReadLine());
+
+                count++;
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower.");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher.");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You guessed it correctly in {count} attempts.");
+                    break;
+                }
+            }
+            // Constructing game loop
+            Console.Write("Do you wish to continue playing (y/n)? ");
+            string userWish = Console.ReadLine().ToLower();
+
+            if (userWish == "n")
+            {
+                Console.WriteLine("Thank you for playing.");
+                break;
+            }
+        }
+        
     }
 }

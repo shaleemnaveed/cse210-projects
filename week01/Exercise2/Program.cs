b5d0ebd4ef28ef6ce6b6b1a79eec9e772c51e63b
shaleemnaveed <shaleemnaveed@gmail.com>
2025-06-30 21:18:07 +0500

    Finished Prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Gets user's grade percentage
+        Console.Write("What is your grade percentage? ");
+        float percentage = float.Parse(Console.ReadLine());
+
+        // Calculate grade
+        string letter;
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Assign signs
+        string sign = "";
+        if (percentage % 10 >= 7)
+        {
+            if (!(letter == "A" || letter == "F"))
+            {
+                sign = "+";
+            }
+        }
+        else if (percentage % 10 < 3)
+        {
+            if (!(letter == "F"))
+            {
+                sign = "-";
+            }
+        }
+
+        // Print user's grade
+        Console.WriteLine($"You scored a grade of {letter}{sign}");
+
+        // Determine whether user passed the class or not
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you failed to pass the class. Better luck next time!");
+        }
     }
 }
